3e9f7d282c9ae3df5229b6a336c4d2b0beb6c436
demon <838532366@qq.com>
2016-08-24 13:43:23 +0800

    init

--- string.go
+++ string.go
@@ -7,6 +7,16 @@ import (
 	"github.com/skip2/go-qrcode"
 )
 
+const (
+	TimePattern1 TimePattern = `2006-01-02 15:04:05`
+	TimePattern2 TimePattern = `2006-01-02 15:04`
+	TimePattern3 TimePattern = `2006-01-02`
+	TimePattern4 TimePattern = `2006/01/02`
+	TimePattern5 TimePattern = `2006/01/02 15:04:05`
+)
+
+type TimePattern string
+
 func IsBlank(src string) bool {
 	if len(src) <= 0 {
 		return true
@@ -29,4 +39,8 @@ func MakeCaptcha() string {
 
 func MakeQrCode(src string) ([]byte,error) {
 	return qrcode.Encode(src, qrcode.Medium, 256)
+}
+
+func FormatTime(time *time.Time,pattern TimePattern) string {
+	return time.Format(pattern)
 }
